6fc390d638e41abb50a95ac5c87733b117a3dd80
wgaonar <wgaonar@gmail.com>
2025-02-19 22:02:54 -0600

    Remove led pin attached to P9_22 that is PWM reserved

--- Chapter01_GPIO/Listing_1.3/Listing_1.3.cpp
+++ Chapter01_GPIO/Listing_1.3/Listing_1.3.cpp
@@ -19,19 +19,16 @@ int main()
   string message = "Main program starting here...";
   cout << RainbowText(message,"Blue", "White", "Bold") << endl;
   
-  GPIO blueLed1(P9_22, OUTPUT);
   GPIO redLed(P8_12, OUTPUT);
   GPIO yellowLed(P8_14, OUTPUT);
   GPIO greenLed(P8_16, OUTPUT);
   GPIO whiteLed(P8_18, OUTPUT);
-  GPIO blueLed2(P8_26, OUTPUT);
+  GPIO blueLed(P8_26, OUTPUT);
 
   char userInput = '\0';
   while (userInput != 'y')
   {
     int time = 100;
-    blueLed1.DigitalWrite(HIGH);
-    DelayMilliseconds(time);
 
     redLed.DigitalWrite(HIGH);
     DelayMilliseconds(time);
@@ -45,10 +42,10 @@ int main()
     whiteLed.DigitalWrite(HIGH);
     DelayMilliseconds(time);
 
-    blueLed2.DigitalWrite(HIGH);
+    blueLed.DigitalWrite(HIGH);
     DelayMilliseconds(time);
 
-    blueLed2.DigitalWrite(LOW);
+    blueLed.DigitalWrite(LOW);
     DelayMilliseconds(time);
 
     whiteLed.DigitalWrite(LOW);
@@ -63,14 +60,11 @@ int main()
     redLed.DigitalWrite(LOW);
     DelayMilliseconds(time);
 
-    blueLed1.DigitalWrite(LOW);
-    DelayMilliseconds(time);
-
-    cout << "Enter 'y', to finish the program: " << endl;
+    cout << "Enter 'y', to finish the program or another key to repeat: " << endl;
     cin >> userInput;
   }
   
   message = "Main program finishes here...";
-  cout << RainbowText(message,"Blue", "Bold") << endl;
+  cout << RainbowText(message,"Blue", "White", "Bold") << endl;
   return 0;
 }
