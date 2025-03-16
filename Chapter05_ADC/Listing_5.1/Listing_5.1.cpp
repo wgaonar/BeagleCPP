ff4a1580aa591ee572726785369617de2a7103c7
wgaonar <wgaonar@gmail.com>
2025-03-16 16:10:44 -0600

    Comments for reference were added

--- Chapter05_ADC/Listing_5.1/Listing_5.1.cpp
+++ Chapter05_ADC/Listing_5.1/Listing_5.1.cpp
@@ -22,7 +22,7 @@ int main()
   cout << RainbowText(message, "Blue") << endl;
   ADC adcPin(P9_39);
 
-  message = "Read an analog value and wait 100 milliseconds between each read";
+  message = "Read an analog value [0-4095] and wait 100 milliseconds between each read";
   cout << RainbowText(message, "Blue") << endl;
 
   int adcValue = 0;
@@ -30,7 +30,7 @@ int main()
   for (int i = 0; i < 100; i++) 
   {
     adcValue = adcPin.ReadADC(intervalTime);
-    cout << "Reading " << i+1 << " of " << "100: " << adcValue << endl;
+    cout << "Reading " << i+1 << " of " << "100: " << adcValue << " [0 - 4095]" << endl;
   }
 
   message = "Main program finishes here...";
@@ -38,3 +38,18 @@ int main()
 
   return 0;
 }
+
+// Bone  | Pocket | AIN
+// ----- | ------ | ---
+// P9_39 | P1_19  | 0
+// P9_40 | P1_21  | 1
+// P9_37 | P1_23  | 2
+// P9_38 | P1_25  | 3
+// P9_33 | P1_27  | 4
+// P9_36 | P2_35  | 5
+// P9_35 | P1_02  | 6
+
+//  AIN    |  Bone  | Pocket | AIN
+// ------- | ------ | -----  | ----
+// VDD_ADC | P9_32  | P1.18  | 1.8V
+// GND_ADC | P9_34  | P1.17  | 0V 
